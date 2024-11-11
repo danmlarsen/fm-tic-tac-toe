7d52135ae74f53b34268e4e7291af1b3c58c5f4d
danmlarsen <larsendanmarius@gmail.com>
2024-11-11 15:25:16 +0100

    Minor adjustments

--- src/components/TurnDisplay.tsx
+++ src/components/TurnDisplay.tsx
@@ -9,7 +9,7 @@ export default function TurnDisplay() {
   const iconClasses = 'size-4 fill-current sm:size-5';
 
   return (
-    <div className="justify-self-center flex items-center justify-center w-24 h-10 sm:h-[3.25rem] sm:w-[8.75rem] pb-1 sm:pb-[6px] sm:text-sm bg-navy-semidark rounded-sm sm:rounded-md shadow-inner-sm shadow-navy-darker text-silver-dark">
+    <div className="justify-self-center flex items-center justify-center w-24 h-10 sm:h-[3.25rem] sm:w-[8.75rem] pb-1 sm:text-sm bg-navy-semidark rounded-sm sm:rounded-md shadow-inner-sm shadow-navy-darker text-silver-dark">
       <h1 className="flex gap-2 sm:gap-[0.8125rem] items-center justify-center">
         <AnimatePresence mode="wait">
           {currentPlayer === 0 ? (
