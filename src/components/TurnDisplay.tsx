dcbe4ec95304052e7276274078f8753ffbad6dec
danmlarsen <larsendanmarius@gmail.com>
2024-11-11 15:17:14 +0100

    Fixing heading warning

--- src/components/TurnDisplay.tsx
+++ src/components/TurnDisplay.tsx
@@ -9,19 +9,21 @@ export default function TurnDisplay() {
   const iconClasses = 'size-4 fill-current sm:size-5';
 
   return (
-    <div className="justify-self-center flex gap-2 sm:gap-[0.8125rem] items-center justify-center w-24 h-10 sm:h-[3.25rem] sm:w-[8.75rem] sm:text-sm bg-navy-semidark rounded-sm sm:rounded-md shadow-inner-sm shadow-navy-darker text-silver-dark">
-      <AnimatePresence mode="wait">
-        {currentPlayer === 0 ? (
-          <motion.div key="x" initial={{ opacity: 0, y: 10 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: 10 }}>
-            <IconX className={iconClasses} />
-          </motion.div>
-        ) : (
-          <motion.div key="o" initial={{ opacity: 0, y: -10 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: -10 }}>
-            <IconO className={iconClasses} />
-          </motion.div>
-        )}
-      </AnimatePresence>
-      <span className="font-bold">Turn</span>
+    <div className="justify-self-center flex items-center justify-center w-24 h-10 sm:h-[3.25rem] sm:w-[8.75rem] sm:text-sm bg-navy-semidark rounded-sm sm:rounded-md shadow-inner-sm shadow-navy-darker text-silver-dark">
+      <h1 className="flex gap-2 sm:gap-[0.8125rem] items-center justify-center">
+        <AnimatePresence mode="wait">
+          {currentPlayer === 0 ? (
+            <motion.span key="x" initial={{ opacity: 0, y: 10 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: 10 }}>
+              <IconX className={iconClasses} />
+            </motion.span>
+          ) : (
+            <motion.span key="o" initial={{ opacity: 0, y: -10 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: -10 }}>
+              <IconO className={iconClasses} />
+            </motion.span>
+          )}
+        </AnimatePresence>
+        <span className="font-bold">Turn</span>
+      </h1>
     </div>
   );
 }
